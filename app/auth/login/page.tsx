5f1b2238ee29b62c789aafef1408afeaca741ec0
DhirajMohata <dhirajmohata86@gmail.com>
2024-12-21 16:50:39 +0530

    store

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,13 +1,31 @@
+"use client"
 
 import { AuthForm } from "@/components/forms/auth-form"
-import { signIn } from "../../actions/signin"
 import { ThemeToggle } from "@/components/themes/themeToggle"
+import { useDispatch, useSelector } from "react-redux"
+import { useRouter } from "next/navigation"
+import { login } from "@/store/slices/authSlice"
+import { RootState } from '@/store/store'
+
 
 export default function SignInPage() {
+  const dispatch = useDispatch()
+  const router = useRouter()
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn)
+
+  const handleSignIn = async (formData: FormData) => {
+    dispatch(login({ email: "email", token: "data_token" }))
+    localStorage.setItem("token", "data_token")
+    router.push("/dashboard") 
+  }
+
+  if(isLoggedIn || localStorage.getItem("token")) {
+    router.push("/dashboard")
+  }
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-purple-50 to-purple-100 dark:from-purple-950 dark:to-purple-900 flex items-center justify-center p-4">
-      <AuthForm mode="signin" action={signIn} />
+      <AuthForm mode="signin" action={handleSignIn} />
       <ThemeToggle />
     </div>
   )
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,13 +1,32 @@
-
+'use client'
 import { AuthForm } from "@/components/forms/auth-form"
-import { signUp } from "../../actions/signup"
 import { ThemeToggle } from "@/components/themes/themeToggle"
+import { useDispatch, useSelector } from "react-redux"
+import { useRouter } from "next/navigation"
+import { login } from "@/store/slices/authSlice"
+import { RootState } from '@/store/store'
 
 export default function SignUpPage() {
+  const dispatch = useDispatch()
+  const router = useRouter()
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn)
+
+
+  const handleSignUp = async (formData: FormData) => {
+
+    dispatch(login({ email: "email", token: "data_token" }))
+    localStorage.setItem("token", "data_token")
+    localStorage.setItem("isFirstTime", "true")
+    router.push("/dashboard") 
+  }
+
+  if(isLoggedIn || localStorage.getItem("token")) {
+    router.push("/dashboard")
+  }
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-purple-50 to-purple-100 dark:from-purple-950 dark:to-purple-900 flex items-center justify-center p-4">
-      <AuthForm mode="signup" action={signUp} />
+      <AuthForm mode="signup" action={handleSignUp} />
       <ThemeToggle />
     </div>
   )
